2153c1dd40a1980fe3f35c360f95fd5b00e20c5b
Deenu143 <deenbandhunetam488@gmail.com>
2023-12-20 16:23:22 +0530

    update codes

--- app/src/main/java/org/deenu/code/fragments/EditorFragment.kt
+++ app/src/main/java/org/deenu/code/fragments/EditorFragment.kt
@@ -7,6 +7,16 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.deenu.code.databinding.FragmentEditorBinding
+import io.github.rosemoe.sora.langs.textmate.registry.FileProviderRegistry
+import io.github.rosemoe.sora.langs.textmate.registry.provider.AssetsFileResolver
+import io.github.rosemoe.sora.langs.textmate.registry.model.ThemeModel
+import io.github.rosemoe.sora.langs.textmate.registry.GrammarRegistry
+import org.eclipse.tm4e.core.registry.IThemeSource
+import io.github.rosemoe.sora.langs.textmate.registry.ThemeRegistry
+import io.github.rosemoe.sora.langs.textmate.TextMateColorScheme
+import io.github.rosemoe.sora.langs.textmate.TextMateLanguage
+import io.github.rosemoe.sora.text.ContentIO
+import java.io.IOException
 
 class EditorFragment : Fragment() {
 	
@@ -32,20 +42,89 @@ class EditorFragment : Fragment() {
 	
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setToolbar()
-        setViews()
+        setToolbar()        
+		loadDefaultThemes()
+        loadDefaultLanguages()
+		ensureTextmateTheme()
+		setViews()
     }
 	
     private fun setToolbar() {
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolBar)
     }
 	
-    private fun setViews() { }
-	    	
+    private fun setViews() { 
+		val editor = binding.editor
+        val language = TextMateLanguage.create(
+            "source.java", true
+        )
+
+        editor.setEditorLanguage(language)
+		openAssetsFile("samples/sample.txt")
+	}
+	
+	private /*suspend*/ fun loadDefaultThemes() /*= withContext(Dispatchers.IO)*/ {
+
+        //add assets file provider
+        FileProviderRegistry.getInstance().addFileProvider(
+            AssetsFileResolver(
+                requireContext().assets
+            )
+        )
+
+        val themes = arrayOf("darcula", "abyss", "quietlight")
+        val themeRegistry = ThemeRegistry.getInstance()
+        themes.forEach { name ->
+            val path = "textmate/$name.json"
+            themeRegistry.loadTheme(
+                ThemeModel(
+                    IThemeSource.fromInputStream(
+                        FileProviderRegistry.getInstance().tryGetInputStream(path), path, null
+                    ), name
+                ).apply {
+                    if (name != "quietlight") {
+                        isDark = true
+                    }
+                }
+            )
+        }
+
+        themeRegistry.setTheme("quietlight")
+    }
+
+    private /*suspend*/ fun loadDefaultLanguages() /*= withContext(Dispatchers.Main)*/ {
+        GrammarRegistry.getInstance().loadGrammars("textmate/languages.json")
+    }
+	
+	private fun ensureTextmateTheme() {
+        val editor = binding.editor
+        var editorColorScheme = editor.colorScheme
+        if (editorColorScheme !is TextMateColorScheme) {
+            editorColorScheme = TextMateColorScheme.create(ThemeRegistry.getInstance())
+            editor.colorScheme = editorColorScheme
+        }
+    }
+	
+	private fun openAssetsFile(name: String) {
+        Thread {
+            try {
+                val text = ContentIO.createFrom(requireContext().assets.open(name))
+                (requireActivity() as AppCompatActivity).runOnUiThread {
+                    binding.editor.setText(text, null)                  
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }.start()        
+    }
+	
     override fun onResume() {
         super.onResume()
-        setToolbar()
-        setViews()
+        setToolbar()        
+		loadDefaultThemes()
+        loadDefaultLanguages()
+		ensureTextmateTheme()
+		setViews()
     }
 
     override fun onDestroyView() {
